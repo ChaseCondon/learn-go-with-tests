f22d168e73981f1157e5cd1f823aa45e986cb07c
Chase Condon <chase.condon@gmail.com>
2025-11-14 16:25:55 +0000

    Added multi-language support and error checking to hello world

--- hello/hello.go
+++ hello/hello.go
@@ -2,10 +2,28 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
-	return fmt.Sprintf("Hello, %s!", name)
+var greetings = map[string]string{
+	"English": "Hello, ",
+	"Spanish": "Hola, ",
+	"French":  "Bonjour, ",
+}
+
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	if language == "" {
+		language = "English"
+	}
+
+	greeting, ok := greetings[language]
+	if ok {
+		return fmt.Sprintf("%s%s!", greeting, name)
+	} else {
+		panic(fmt.Sprintf("%s is not a supported language", language))
+	}
 }
 
 func main() {
-	fmt.Println(Hello("Chase"))
+	fmt.Println(Hello("Chase", ""))
 }
